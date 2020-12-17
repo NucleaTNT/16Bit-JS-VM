e216f0a076ef2d816eb2f7911a813013d70a6af7
NucleaTNT <47010027+NucleaTNT@users.noreply.github.com>
2020-12-17 19:22:34 +0000

    Added Memory Bank

--- Index.js
+++ Index.js
@@ -3,3 +3,60 @@ const CPU = require("./CPU");
 const MemoryMapper = require("./MemoryMapper");
 
 const MM = new MemoryMapper();
+
+const dataViewMethods = [
+    "getUint8",
+    "setUint8",
+    "getUint16",
+    "setUint16"
+]
+
+const CreateBankedMemory = (n, bankSize, cpu) => {
+    const bankBuffers = Array.from({length: n}, () => new ArrayBuffer(bankSize));
+    const banks = bankBuffers.map(ab => new DataView(ab));
+
+    const forwardToDataView = name => (...args) => {
+        const bankIndex = cpu.GetRegister("mb") % n;
+        const memoryBankToUse = banks[bankIndex];
+        return memoryBankToUse[name](...args);
+    };
+
+    const interface = dataViewMethods.reduce((dvOut, fnName) => {
+        dvOut[fnName] = forwardToDataView(fnName);
+        return dvOut;
+    }, {});
+
+    return interface;
+}
+
+const bankSize = 0xff;
+const nBanks = 8;
+const cpu = new CPU(MM);
+
+const memoryBankDevice = CreateBankedMemory(nBanks, bankSize, cpu);
+MM.map(memoryBankDevice, 0, bankSize);
+
+const regularMemory = CreateMemory(0xff00);
+MM.map(regularMemory, bankSize, 0xffff, true);
+
+console.log("Writing value 1 to address 0");
+MM.setUint16(0, 1);
+console.log("Reading value at address 0:", MM.getUint16(0));
+
+console.log("\n::: Switching memory bank (0 -> 1)");
+cpu.SetRegister("mb", 1);
+console.log("Reading value at address 0:", MM.getUint16(0));
+
+console.log("Writing value 42 to address 0");
+MM.setUint16(0, 42);
+console.log("\n::: Switching memory bank (1 -> 2)");
+cpu.SetRegister("mb", 2);
+console.log("Reading value at address 0:", MM.getUint16(0));
+
+console.log("\n::: Switching memory bank (2 -> 1)");
+cpu.SetRegister("mb", 1);
+console.log("Reading value at address 0:", MM.getUint16(0));
+
+console.log("\n::: Switching memory bank (1 -> 0)");
+cpu.SetRegister("mb", 0);
+console.log("Reading value at address 0:", MM.getUint16(0));
--- MemoryMapper.js
+++ MemoryMapper.js
@@ -3,7 +3,7 @@ class MemoryMapper {
         this.regions = [];
     }
 
-    Map(device, start, end, remap = true) {
+    map(device, start, end, remap = true) {
         const region = {
             device,
             start,
@@ -17,7 +17,7 @@ class MemoryMapper {
         };
     }
 
-    FindRegion(address) {
+    findRegion(address) {
         let region = this.regions.find(r => address >= r.start && address <= r.end);
         if (!region) {
             throw new Error(`No memory region found for address ${address}`);
@@ -26,25 +26,25 @@ class MemoryMapper {
     }
 
     getUint8(address) {
-        const region = this.FindRegion(address);
+        const region = this.findRegion(address);
         const finalAdress = region.remap ? address - region.start : address;
         return region.device.getUint8(finalAdress);
     }
 
     setUint8(address, value) {
-        const region = this.FindRegion(address);
+        const region = this.findRegion(address);
         const finalAdress = region.remap ? address - region.start : address;
         return region.device.setUint8(finalAdress, value);
     }
 
     getUint16(address) {
-        const region = this.FindRegion(address);
+        const region = this.findRegion(address);
         const finalAdress = region.remap ? address - region.start : address;
         return region.device.getUint16(finalAdress);
     }
 
     setUint16(address, value) {
-        const region = this.FindRegion(address);
+        const region = this.findRegion(address);
         const finalAdress = region.remap ? address - region.start : address;
         return region.device.setUint16(finalAdress, value);
     }
